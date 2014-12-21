419012355974d81feef5e8b562c18cd75b6c8b0f
Sarah <nachochips2@gmail.com>
2014-12-20 22:12:43 -0800

    Started on model for Board and Obstacle class

--- src/com/example/ButtonGames/model/Board.java
+++ src/com/example/ButtonGames/model/Board.java
@@ -1,7 +1,58 @@
 package com.example.ButtonGames.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Sarah on 2014-12-20.
  */
 public class Board {
+
+    private Player playerR;
+    private Player playerL;
+    private final int HEIGHT = 1920;
+    private final int WIDTH = 1080;
+    private List<Obstacle> obstacles;
+
+
+    public Board(Player playerR, Player playerL){
+        playerR = this.playerR;
+        playerL = this.playerL;
+        obstacles = new ArrayList<Obstacle>();
+    }
+
+    public void addObstacle(Obstacle o){
+        obstacles.add(o);
+    }
+
+    public boolean removeObstacle(Obstacle o){
+        return obstacles.remove(o);
+    }
+
+    public List<Obstacle> getObstacles(){
+        return obstacles;
+    }
+
+    // Produce true if can move to that x, y coordinate
+    public boolean canMove(Double x, Double y){
+        boolean insideObstacle = false;
+        for (Obstacle o: obstacles){
+            if (o.getXRange().contains(x) && o.getYRange().contains(y)){
+                insideObstacle = true;
+            }
+        }
+        return !insideObstacle;
+    }
+
+    public void switchRoles(){
+        playerR.setState(playerL.getState());
+        playerL.setState(!playerL.getState());
+        }
+
+    public void updateBoard(){
+        playerL.action();
+        playerR.action();
+    }
+
+
 }
---
+++ src/com/example/ButtonGames/model/Obstacle.java
@@ -0,0 +1,26 @@
+package com.example.ButtonGames.model;
+
+import android.util.Range;
+
+/**
+ * Created by Sarah on 2014-12-20.
+ */
+public class Obstacle {
+
+    private Range<Double> xRange;
+    private Range<Double> yRange;
+
+    public Obstacle (Double xL, Double xU, Double yL, Double yU){
+        xRange = new Range<Double>(xL, xU);
+        yRange = new Range<Double>(yL, yU);
+    }
+
+    public Range<Double> getXRange(){
+        return xRange;
+    }
+
+    public Range<Double> getYRange(){
+        return yRange;
+    }
+
+}
