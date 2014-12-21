68877bf3d87f132e3279e3d19082a5692be5fd4c
Sarah <nachochips2@gmail.com>
2014-12-21 15:53:22 -0800

    SimpleTagActivity added

---
+++ src/com/example/ButtonGames/activity/SimpleTagActivity.java
@@ -0,0 +1,14 @@
+package com.example.ButtonGames.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.example.ButtonGames.R;
+
+
+public class SimpleTagActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game);
+    }
+}
