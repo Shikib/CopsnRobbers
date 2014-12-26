872f14feebb4aaa904e69a00ecfc94d85153585f
Shikib <mehrishikib@gmail.com>
2014-12-26 14:39:31 -0800

    Chosen theme is no longer erased when the application is closed.

--- src/com/example/ButtonGames/activity/SelectionMenuActivity.java
+++ src/com/example/ButtonGames/activity/SelectionMenuActivity.java
@@ -1,20 +1,36 @@
 package com.example.ButtonGames.activity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import com.example.ButtonGames.R;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 /**
  * Created by Sarah on 2014-12-25.
  */
 public class SelectionMenuActivity extends Activity{
+
+    private int theme;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        SharedPreferences prefs = getApplicationContext().getSharedPreferences(
+                "com.example.ButtonGames", Context.MODE_PRIVATE);
+
+        theme = prefs.getInt("theme", 0);
+
 
         // Get rid of banner, fill screens the app
         requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -32,24 +48,35 @@ public class SelectionMenuActivity extends Activity{
         setContentView(R.layout.selection_menu);
     }
 
+    public static String convertStreamToString(InputStream is) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+            sb.append(line).append("\n");
+        }
+        reader.close();
+        return sb.toString();
+    }
+
     public void onMap0Button(View view){
         Intent i = new Intent(this, SimpleTagActivity.class);
         i.putExtra("com.example.ButtonGames.obstacle", 0);
-        i.putExtra("com.example.ButtonGames.theme", SettingsMenuActivity.theme);
+        i.putExtra("com.example.ButtonGames.theme", theme);
         startActivity(i);
         finish();
     }
     public void onMap1Button(View view){
         Intent i = new Intent(this, SimpleTagActivity.class);
         i.putExtra("com.example.ButtonGames.obstacle", 1);
-        i.putExtra("com.example.ButtonGames.theme", SettingsMenuActivity.theme);
+        i.putExtra("com.example.ButtonGames.theme", theme);
         startActivity(i);
         finish();
     }
     public void onMap2Button(View view){
         Intent i = new Intent(this, SimpleTagActivity.class);
         i.putExtra("com.example.ButtonGames.obstacle", 2);
-        i.putExtra("com.example.ButtonGames.theme", SettingsMenuActivity.theme);
+        i.putExtra("com.example.ButtonGames.theme", theme);
         startActivity(i);
         finish();
     }
--- src/com/example/ButtonGames/activity/SettingsMenuActivity.java
+++ src/com/example/ButtonGames/activity/SettingsMenuActivity.java
@@ -2,6 +2,7 @@ package com.example.ButtonGames.activity;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -9,15 +10,18 @@ import android.view.WindowManager;
 import android.widget.Toast;
 import com.example.ButtonGames.R;
 
+import java.io.*;
+
 /**
  * Created by Sarah on 2014-12-25.
  */
 public class SettingsMenuActivity extends Activity {
-    public static int theme = 0; // default theme
+    public static int theme; // default theme
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         // Get rid of banner, fill screens the app
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
@@ -36,18 +40,22 @@ public class SettingsMenuActivity extends Activity {
 
     public void onTheme0Button(View view){
         theme = 0;
+        chooseTheme(theme);
         makeToast("Stone theme set");
     }
     public void onTheme1Button(View view){
         theme = 1;
+        chooseTheme(theme);
         makeToast("Sand theme set");
     }
     public void onTheme2Button(View view){
         theme = 2;
+        chooseTheme(theme);
         makeToast("Grass theme set");
     }
     public void onTheme3Button(View view){
         theme = 3;
+        chooseTheme(theme);
         makeToast("Bamboo theme set");
     }
 
@@ -57,6 +65,15 @@ public class SettingsMenuActivity extends Activity {
 
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
+    }
+
+    public void chooseTheme(int theme) {
+        SharedPreferences prefs = getApplicationContext().getSharedPreferences(
+                "com.example.ButtonGames", Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt("theme", theme);
+        editor.commit();
     }
 
     public void onBackButton(View view){
